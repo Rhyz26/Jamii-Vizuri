b6ba59938319075f21fd85cb8417bbae02c9fc54
Raymond <njagala.raymond@gmail.com>
2023-11-27 17:33:09 +0300

    Fixed an error, the import of signup wasnt rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import '@mantine/core/styles.css';
 
 import { MantineProvider } from '@mantine/core';
-import SignUp from "./components/signup";
+import SignUp from './components/Sign';
 import { useState } from 'react';
 import { Input, CloseButton } from '@mantine/core';
 import { IconAt } from '@tabler/icons-react';
