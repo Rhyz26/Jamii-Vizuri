8e1e2c3e58fe2ee08b58919da2959f3ccc9e550c
Raymond <njagala.raymond@gmail.com>
2023-12-01 02:16:20 +0300

    Added a blogs page, deleted a few code from the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Home from "./components/Home";
+import Blog from "./components/Blog";
 import Header from "./components/Header";
 import "@mantine/core/styles.css";
 import Footer from "./components/Footer";
@@ -13,41 +13,11 @@ function App() {
        <div>
           <Header />
         </div>
+        <div>
+          <Blog/>
+        </div>
 
-        <h1 className="text-6xl">Welcome to Jamii Vizuri Community Health Services </h1>
-
-      <div className="flex justify-center gap-8 pb-8 pt-4">
-        <Image src="https://images.pexels.com/photos/159935/pexels-photo-159935.jpeg?auto=compress&cs=tinysrgb&w=800" w={500}/>
-        <Image src="https://news.mit.edu/sites/default/files/images/202304/Uganda-Vaccination-Campaign-cover.jpg" w={500}/>
-      </div>
-
-
-<div className="flex justify-center space-x-16 pb-4">
-
-      <div className="flex flex-col w-60 bg-lime-700 rounded-2xl">
-        <Image src="https://cdn.pixabay.com/photo/2017/01/13/01/22/ok-1976099_1280.png"w={100}/>
-        <h1 className="text-lg font-bold flex-wrap text-center">I am i Eligible</h1>
-        <p>I would love to become a patient</p>
-      </div>
-
-      <div className="flex flex-col bg-amber-700 w-60 rounded-2xl">
-        <Image src="https://cdn.pixabay.com/photo/2020/10/20/19/12/elderly-5671352_1280.png" w={100}/>
-        <h1 className="font-bold text-lg text-center">Volunteer</h1>
-        <p>I would love to get involved</p>
-      </div>
-
-      <div className="flex flex-col bg-fuchsia-800 w-60 rounded-2xl">
-        <Image src="https://mymbchc.org/wp-content/uploads/2021/02/banner-image-1-1-1.png" w={100}/>
-        <h1 className="text-lg font-bold text-center">Patient Resources</h1>
-        <p>I am looking for health Information</p>
-      </div>
-</div>
-
-
-      <div>
-       
-        <Home />
-      </div>
+      
       <Footer/>
     </MantineProvider>
   );
---
+++ src/components/Blog.jsx
@@ -0,0 +1,8 @@
+function Blog(){
+    return(
+        <div>
+            <h1 className="font-bold text-lg">Blog</h1>
+        </div>
+    )
+}
+export default Blog;
